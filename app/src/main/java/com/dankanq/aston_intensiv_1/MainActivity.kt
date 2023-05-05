05c2bfbd9deb2be471c5b30b328b973800fa6f9c
Yoru <taihoudev@gmail.com>
2023-05-05 15:16:17 +0300

    Initial commit

---
+++ app/src/main/java/com/dankanq/aston_intensiv_1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dankanq.aston_intensiv_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
